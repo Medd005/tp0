795482793d01d002bf28e345dd47f468f390eb56
Mederic Lavallee <2390465@etu.cegep-lanaudiere.qc.ca>
2024-09-24 10:52:47 -0400

    debut tp

---
+++ tp0.py
@@ -0,0 +1,30 @@
+
+
+def menu()
+choix: int
+choixValide: bool = False
+
+print ("Bienvenue au jeu du nombre mystère !")
+print("Facile(1): Nombre entre 1 et 19, 10 essais")
+print("Normal(2): Nombre entre 1 et 49, 5 essais")
+print("Difficile(3): Nombre entre 1 et 99, 5 essais")
+print("Impossible(4): Nombre entre 1 et 999, 1 essai")
+
+choix = int(input("Veuillez entrer votre choix (1, 2, 3 ou 4): "))
+match choix:
+    case 1:
+
+
+    case 2:
+
+
+    case 3:
+
+
+    case 4:
+
+
+
+
+
+
